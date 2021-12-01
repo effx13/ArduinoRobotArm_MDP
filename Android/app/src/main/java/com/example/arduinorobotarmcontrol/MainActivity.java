bcb886a012c151f34b9729057e1825941dc734cf
eFFx <chanoo104@naver.co>
2021-12-01 12:29:07 +0900

    feat: 안드로이드 블루투스 페어링 리스트뷰 삭제

--- Android/app/src/main/java/com/example/arduinorobotarmcontrol/MainActivity.java
+++ Android/app/src/main/java/com/example/arduinorobotarmcontrol/MainActivity.java
@@ -9,7 +9,6 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ListView;
 import android.widget.SeekBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -28,21 +27,20 @@ public class MainActivity extends AppCompatActivity {
     BluetoothAdapter btAdapter;
     TextView textStatus;
 
-    ListView listView;
+    //ListView listView;
     private final static int REQUEST_ENABLE_BT = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        listView = (ListView) findViewById(R.id.listview);
+
         SeekBar seekBarX = findViewById(R.id.seekBar_X);
         SeekBar seekBarY = findViewById(R.id.seekBar_Y);
         SeekBar seekBarZ = findViewById(R.id.seekBar_Z);
         TextView X_Value = findViewById(R.id.XValue);
         TextView Y_Value = findViewById(R.id.YValue);
         TextView Z_Value = findViewById(R.id.ZValue);
-        Button btnPaired = findViewById(R.id.btn_paired);
         Button submit_button = findViewById(R.id.change_button);
 
         btAdapter = BluetoothAdapter.getDefaultAdapter();
@@ -51,12 +49,12 @@ public class MainActivity extends AppCompatActivity {
         seekBarY.setOnSeekBarChangeListener(new SeekbarChanged(Y_Value));
         seekBarZ.setOnSeekBarChangeListener(new SeekbarChanged(Z_Value));
 
-        btnPaired.setOnClickListener(this::onClickButtonPaired);
-        listView.setOnItemClickListener(new myOnItemClickListener());
+
+//        listView.setOnItemClickListener(new myOnItemClickListener());
 
         btArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
         deviceAddressArray = new ArrayList<>();
-        listView.setAdapter(btArrayAdapter);
+        //listView.setAdapter(btArrayAdapter);
         submit_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -82,39 +80,39 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-    public class myOnItemClickListener implements AdapterView.OnItemClickListener {
-
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            Toast.makeText(getApplicationContext(), btArrayAdapter.getItem(position), Toast.LENGTH_SHORT).show();
-
-            textStatus.setText("try...");
-
-            final String name = btArrayAdapter.getItem(position); // get name
-            final String address = deviceAddressArray.get(position); // get address
-            boolean flag = true;
-
-            BluetoothDevice device = btAdapter.getRemoteDevice(address);
-
-            // create & connect socket
-            try {
-                btSocket = createBluetoothSocket(device);
-                btSocket.connect();
-            } catch (IOException e) {
-                flag = false;
-                textStatus.setText("connection failed!");
-                e.printStackTrace();
-            }
-
-            if(flag){
-                textStatus.setText("connected to "+name);
-                connectedThread = new ConnectedThread(btSocket);
-                connectedThread.start();
-            }
-
-        }
-
-    }
+//    public class myOnItemClickListener implements AdapterView.OnItemClickListener {
+//
+//        @Override
+//        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//            Toast.makeText(getApplicationContext(), btArrayAdapter.getItem(position), Toast.LENGTH_SHORT).show();
+//
+//            textStatus.setText("try...");
+//
+//            final String name = btArrayAdapter.getItem(position); // get name
+//            final String address = deviceAddressArray.get(position); // get address
+//            boolean flag = true;
+//
+//            BluetoothDevice device = btAdapter.getRemoteDevice(address);
+//
+//            // create & connect socket
+//            try {
+//                btSocket = createBluetoothSocket(device);
+//                btSocket.connect();
+//            } catch (IOException e) {
+//                flag = false;
+//                textStatus.setText("connection failed!");
+//                e.printStackTrace();
+//            }
+//
+//            if(flag){
+//                textStatus.setText("connected to "+name);
+//                connectedThread = new ConnectedThread(btSocket);
+//                connectedThread.start();
+//            }
+//
+//        }
+//
+//    }
 }
 
 
